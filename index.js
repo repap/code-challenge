306d00f07dfcbd4627b077a7181b569b98bf2aaa
Dennis Penning <penning.dennis@googlemail.com>
2019-08-06 06:35:03 +0200

    add first route handler add query for amount and book query

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ const app = express()
 
 const {PORT} = process.env
 
+
+// TODO: refactor -> seperate file -> new router
+const filterAmount = num => (e, i) => num > i ? e : false
+
+app.get('/books', (req, res) => {
+  const amount = req.query.amount || 5
+  const q = req.query.q
+  
+  // TODO: add fetch -> get books
+  const data = [1,2,3,4,5,6,7,8]
+
+  // TODO: add tests for amount -> is number
+
+  // TODO: add test query -> is set
+  // TODO: add test query -> is evil
+
+  res.status(200).json({
+    message: '...',
+    query: {
+      q: q,
+      amount: amount
+    },
+    data: data.filter(filterAmount(amount))
+  })
+})
+
+app.use('*', (req, res) =>
+  res.status(400).json({
+    message: 'query is not available',
+    data: []
+  }))
+
+
 app.listen(
   PORT, 
   err => 
